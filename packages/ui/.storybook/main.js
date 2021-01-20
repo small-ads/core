f9a631826c97c34b78613b225dc257bbad8675e7
AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>
2021-01-20 15:17:59 +0100

    feat: :sparkles: Adds storybook (#11)
    * feat: :sparkles: Adds storybook
    
    Installs Storybook initialized with Typescript, create-react-app was also installed using a Typescript template
    
    * refactor: :recycle: deletes unnecessary files, changes name of the project folder
    
    * deletes unecessary Test.stories.mdx file
    
    * docs: :memo: changes the name of the project in the package-json
    
    * docs: :memo: renames folder app
    
    * deletes .keepme unecessary file

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+};
