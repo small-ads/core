6e35a1dec42629a0b0c834b657c436a6671f86a0
AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>
2021-01-22 16:34:37 +0100

    feat: :sparkles: adds styled-components (#13)
    * feat: :sparkles: adds styled-components
    
    Adds styled components from `npm install --save styled-components` and also adds support for Typescript with `npm install @types/styled-components`. To do: Maybe install a babel plugin (`npm install --save-dev babel-plugin-styled-components`) or a a Typescript plugin (`npm i typescript-plugin-styled-components --dev`). I didn't do it because there are issues opened and apparently all those pluggins don't work together.
    
    * refactor: :construction: adds the Button story using styled-components
    
    Only refactors the Button story to first check if that is the way of doing Header and Page.
    
    * refactor: :construction: solves `type: any` issue on ButtonProps interface
    
    eliminates unecessary Page and Header stories, uninstall unneeded dependency storybook/controls dependency. Refactors the Colors object .
    
    * fix: :bug: fixes type declaration on the ui-button
    
    also removes some constants exported as default.

---
+++ packages/ui/src/stories/Button/button.style.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import { Colors } from "../../Colors";
+
+export const ButtonStyles = styled.div`
+  button:disabled,
+  button:disabled .component-child {
+    cursor: not-allowed !important;
+  }
+  button {
+    font-size: 1.6rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    cursor: pointer;
+    font-weight: 400;
+    line-height: 1.9rem;
+    transition: background-color ease 0.2s;
+    height: 50px;
+    min-width: 200px;
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 5px 10px 0px;
+    padding: 0;
+    border-radius: 5px;
+    text-decoration: none;
+  }
+  .primary {
+    color: ${Colors.white};
+    background-color: ${Colors.primary};
+    border: 0;
+    i {
+      font-size: 1.5em;
+    }
+    &:hover {
+      top: -4px;
+      box-shadow: 0 30px 60px rgba(0, 0, 0, 0.12);
+      background-color: ${Colors.active};
+    }
+    &:active {
+      background-color: ${Colors.active};
+    }
+    &:disabled {
+      box-shadow: rgba(0, 0, 0, 0.12) 0px 5px 10px 0px;
+      background-color: ${Colors.divider};
+    }
+  }
+  .secondary {
+    box-sizing: border-box;
+    color: ${Colors.white};
+    border: 1px solid ${Colors.primary};
+    background-color: ${Colors.primary};
+    i {
+      font-size: 1.4em;
+    }
+    &:hover,
+    &:active {
+      color: ${Colors.primary};
+      background-color: ${Colors.white};
+      border: 1px solid ${Colors.primary};
+    }
+    &:disabled {
+      color: ${Colors.white};
+      background-color: ${Colors.secondary};
+      border: 1px solid ${Colors.secondary};
+    }
+  }
+`;
+
+
---
+++ packages/ui/src/stories/Button/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { ButtonStyles } from "./button.style";
+
+export interface ButtonProps {
+  type: "primary" | "secondary";
+  id: string;
+  htmlType: "button" | "submit" | "reset" | undefined;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled: boolean | undefined;
+  style: object;
+  className: string;
+  children: React.ReactNode;
+}
+
+export function Button({
+  type,
+  id,
+  htmlType,
+  onClick,
+  children,
+  disabled,
+  style,
+  className,
+}: ButtonProps) {
+  return (
+    <ButtonStyles>
+      <button
+        type={htmlType}
+        id={id}
+        disabled={disabled}
+        onClick={onClick}
+        className={`${className} ${type}`}
+        style={style}>
+        {children}
+      </button>
+    </ButtonStyles>
+  );
+}
+
+Button.defaultProps = {
+  id: null,
+  onClick: () => {},
+  type: "primary",
+  htmlType: "button",
+  style: {},
+  disabled: false,
+  className: "",
+};
