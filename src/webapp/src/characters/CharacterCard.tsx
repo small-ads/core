50dc39fa1044f0ea77ee4b1a0274025c8fa7c722
AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>
2021-02-07 10:31:24 +0100

    feat: :art: add styled-components (#34)

--- src/webapp/src/characters/CharacterCard.tsx
+++ src/webapp/src/characters/CharacterCard.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface Props {
   name: string;
   species: string;
   image: string;
 }
+const Card = styled.div`
+  background: black;
+  border: 3px solid rgba(255, 255, 255, 1);
+  border-radius: 30px;
+  color: white;
+  box-shadow: 0px 0px 35px 1px rgba(255, 255, 255, 0.8);
+  margin: 3rem;
+  overflow: hidden;
+  position: relative;
+  max-width: 300px;
+`;
+
+const CardFooter = styled.div`
+  padding: 1.5rem;
+`;
+
+const PictureWrap = styled.div`
+  height: 100%;
+  object-fit: cover;
+  width: 100%;
+`;
+
+const CharacterName = styled.h3`
+  margin: 0;
+`;
 
 export const CharacterCard = ({ name, species, image }: Props) => (
-  <div className='card'>
-    <div className='picture-wrap'>
+  <Card>
+    <PictureWrap>
       <img src={image} title={name} alt='character' />
-    </div>
-    <div className='card-footer'>
-      <h3 className='character-name'>{name}</h3>
-      <div>{species}</div>
-    </div>
-  </div>
+      <CardFooter>
+        <CharacterName>{name}</CharacterName>
+        <div>{species}</div>
+      </CardFooter>
+    </PictureWrap>
+  </Card>
 );
