03e94ac336b6934f3f2db3bb82878dc6f643b91a
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-02-09 23:04:58 +0100

    fix: :bug: minor linter error

--- packages/ui/dist/components/Button/index.d.ts
+++ packages/ui/dist/components/Button/index.d.ts
@@ -1,11 +1,20 @@
 import React from 'react';
+
 export interface ButtonProps {
-    type?: 'primary' | 'secondary';
-    submit?: boolean;
-    disabled?: boolean;
-    style?: object;
-    className?: string;
-    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    children: React.ReactNode;
+  type?: 'primary' | 'secondary';
+  submit?: boolean;
+  disabled?: boolean;
+  style?: object;
+  className?: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
 }
-export declare function Button({ type, submit, disabled, style, className, children, onClick, }: ButtonProps): JSX.Element;
+export declare function Button({
+  type,
+  submit,
+  disabled,
+  style,
+  className,
+  children,
+  onClick,
+}: ButtonProps): JSX.Element;
