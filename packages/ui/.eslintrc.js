00bf787f1a578fd47486d687bbd7318b56c5a57d
AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>
2021-02-26 18:44:42 +0100

    ci: add typechecking and caching to Github Actions (#38)
    * ci: remove boostraping on Github actions
    
    * ci: change `yarn install` for `yarn install --prod` on Github Actions
    
    * ci: :bug: remove --prod flag to avoid error on Lint step from pr.yml Action
    
    also add a production.yml
    
    * ci: use `yarn install --prod` in another job from the pr.yml
    
    * ci: :construction_worker: delete unnecessary production.yml file
    
    * ci: :construction_worker: add caching for lint job on Github Actions
    
    * ci: :construction_worker: test caching functionality
    
    * ci: :construction_worker: add typechecking on Github Actions
    
    * fix: :bug: add new line at the end of the pr.yml
    
    * Update pr.yml
    
    * ci: update typecheck configuration
    
    * ci: :construction_worker: add "fake" types for the "@small-ads/ui" on src folder
    
    * fix: :construction_worker: remove fake type declaration from the src folder
    
    * ci: :construction_worker: add `@small-ads/ui` to the types array in tsconfig.json
    
    * ci: :construction_worker: update typeccheck to transile using the tsconfig.build.json
    
    * ci: :construction_worker: use command `tsc` when running the script `typeckeck`
    
    * ci: :construction_worker: desesperetly adding manually the dist folder from the `ui` on the `webapp`
    
    * fix: :construction_worker: typo on folder name
    
    * ci: :construction_worker: delete dist folder from the webapp
    
    * chore: builds the package before typechecking
    
    * chore: Build
    
    * chore: s
    
    * chore: ignores the dist
    
    Co-authored-by: paschalidi <paschalidi@outlook.com>

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
+  ignorePatterns: ['dist/**/*'],
   rules: {
     'import/no-extraneous-dependencies': 'off',
   },
