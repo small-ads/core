835e80bc5414292c864751d715bb7e82a0c86c4e
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-09 22:43:18 +0100

    refactor: delete old components

--- src/webapp/src/reducers.ts
+++ src/webapp/src/reducers.ts
@@ -1,11 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { userReducer } from './auth/userSlice';
-import { charactersReducer } from './characters/charactersSlice';
-import { personReducer } from './person/personSlice';
 
 export const rootReducer = combineReducers({
-  person: personReducer,
-  characters: charactersReducer,
   user: userReducer,
 });
 
